c292ad9eafceedccfec617e096d0c52c7815c1b0
kristof de spiegeleer <kristof@incubaid.com>
2021-07-25 07:38:30 +0000

    ...

--- gitpod_scripts/yggdrasil.sh
+++ gitpod_scripts/yggdrasil.sh
@@ -3,11 +3,12 @@ export DUMB_INIT_VERSION=1.2.2
 export YGGDRASIL_VERSION=0.3.15
 
 git clone "https://github.com/yggdrasil-network/yggdrasil-go.git" /tmp/src
-cd /tmp/src
+pushd /tmp/src
 
 git reset --hard v${YGGDRASIL_VERSION}
 ./build
 sudo cp yggdrasil* /usr/bin/
+popd
 # curl -sSfLo /tmp/dumb-init "https://github.com/Yelp/dumb-init/releases/download/v${DUMB_INIT_VERSION}/dumb-init_${DUMB_INIT_VERSION}_amd64"
 
 # chmod 0755 /tmp/dumb-init

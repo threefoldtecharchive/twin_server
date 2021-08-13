cb12898610d78540492392ba58b33d5cfc3311cc
kristof de spiegeleer <kristof@incubaid.com>
2021-08-13 03:08:25 +0000

    ...

--- .gitpod.Dockerfile
+++ .gitpod.Dockerfile
@@ -5,15 +5,15 @@ FROM gitpod/workspace-full:latest
 USER root
 
 
-RUN apt-get update && apt-get install -y mc rsync 
+RUN apt-get update && apt-get install -y mc rsync redis-server
 RUN apt-get install -y curl musl-tools nano iputils-ping procps iproute2 imagemagick sed build-essential libsodium-dev redis-server tmux
 
-ADD gitpod_scripts/* /tmp/
+# ADD gitpod_scripts/* /tmp/
 
 # RUN bash /tmp/yggdrasil.sh
 
 #prepare for gitpod nodejs env
-RUN bash /tmp/nodejs.sh
+# RUN bash /tmp/nodejs.sh
 
 # RUN apt-get clean && rm -rf /var/cache/apt/* && rm -rf /var/lib/apt/lists/* && rm -rf /tmp/*
 
--- gitpod_scripts/nodejs.sh
+++ gitpod_scripts/nodejs.sh
@@ -1,26 +1,26 @@
 set -ex
-sudo chown gitpod:gitpod -R /tmp
+# sudo chown gitpod:gitpod -R /tmp
 
-export base=/workspace/nodejs
-export lts=14.17.3
+# export base=/workspace/nodejs
+# export lts=14.17.3
 
-echo $base
-sudo mkdir -p $base
-sudo chown gitpod:gitpod -R $base
-cd $base
+# echo $base
+# sudo mkdir -p $base
+# sudo chown gitpod:gitpod -R $base
+# cd $base
 
-rm -f $base/nvm.sh
-curl -s -o nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/nvm.sh
-bash nvm.sh
+# rm -f $base/nvm.sh
+# curl -s -o nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/master/nvm.sh
+# bash nvm.sh
 
-export NVM_DIR=$base
-source $base/nvm.sh
+# export NVM_DIR=$base
+# source $base/nvm.sh
 
-nvm install $lts
-nvm use $lts
+# nvm install $lts
+# nvm use $lts
 
-npm install --global yarn
-npm install pm2 -g
+# npm install --global yarn
+# npm install pm2 -g
 
 
 

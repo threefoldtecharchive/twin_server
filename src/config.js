20652282fc2befab65ef4c06881bc04dbed5e73c
Mohammed Essam <mohammedelborolossy@gmail.com>
2021-08-18 15:41:04 +0000

    Fix config

--- src/config.js
+++ src/config.js
@@ -9,24 +9,6 @@ async function load(){
         config = JSON.parse(fs.readFileSync('config.json'));
         config.publishtools.root = await utils.resolvePath(config.publishtools.root)
         config.hyperdrive.path = await utils.resolvePath(config.hyperdrive.path)
-        config.nodejs.production = process.env.NODE_ENV == 'production'
-
-        var ssl = process.env.ENABLE_SSL || ''
-        config.nodejs.ssl = ssl.toLowerCase() == 'true' || false
-        
-        if(config.nodejs.production){
-            var passPhrase = process.env.THREEBOT_PHRASE
-            var secret = process.env.SECRET
-
-            if(!passPhrase){
-                throw new Error("THREEBOT_PHRASE Env variable must be set for production")
-            }
-            if(!secret){
-                throw new Error("SECRET Env variable must be set for production")
-            }
-            config.threebot.passPhrase = passPhrase
-            config.http.session.secret = secret
-        }
     }catch(e){
         console.log(chalk.red('X (Config) could not be loaded'))
         console.log(e)
